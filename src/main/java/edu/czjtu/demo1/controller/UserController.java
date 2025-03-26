8b39ae8e3e9aff3b871901ec9c1161d6e74b7207
fengmingweiyu <3353085918@qq.com>
2025-03-26 10:51:30 +0800

    添加地址管理功能

--- src/main/java/edu/czjtu/demo1/controller/UserController.java
+++ src/main/java/edu/czjtu/demo1/controller/UserController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class  UserController {
 
     @Autowired
     private UserService userService;
